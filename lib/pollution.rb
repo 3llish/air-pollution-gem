761f27e7b1e2df692e7626d998bd385441e03abb
3llish <coos.dev@gmail.com>
2019-12-28 23:09:28 +0100

    commented and updated

--- lib/pollution.rb
+++ lib/pollution.rb
@@ -1,13 +1,20 @@
 require 'net/http'
 require 'json'
 
-class AirGem
+class Pollution
 	
 	@token
 
+	##
+	# => Initialize the Pollution class and set the token variable.
+	##
 	def initialize(token)
 		@token = token
 	end
+
+	##
+	# => Returns the air pollution data of the given city or throws an error if the city wasn't found.
+	##
 
 	def city(cityname)
 		data = JSON.parse(Net::HTTP.get('api.waqi.info', "/feed/#{cityname}/?token=#{@token}"))
@@ -18,6 +25,10 @@ class AirGem
 		end
 	end
 
+	##
+	# => Returns the air pollution data of the current location (IP Based).
+	##
+
 	def here
 		data = JSON.parse(Net::HTTP.get('api.waqi.info', "/feed/here/?token=#{@token}"))
 		if data['status'] == 'error'
@@ -27,14 +38,9 @@ class AirGem
 		end
 	end
 
-	def geo(latitude, longitude)
-		data = JSON.parse(Net::HTTP.get('api.waqi.info', "/feed/geo:#{latitude};#{longitude}/?token=#{@token}"))
-		if data['status'] == 'error'
-			raise data['data']
-		else
-			return data
-		end
-	end
+	##
+	# => Returns the air pollution data of the given coordinates.
+	##
 
 	def geo(latitude, longitude)
 		data = JSON.parse(Net::HTTP.get('api.waqi.info', "/feed/geo:#{latitude};#{longitude}/?token=#{@token}"))
@@ -44,6 +50,10 @@ class AirGem
 			return data
 		end
 	end
+
+	##
+	# => Returns the air pollution data of the area between the given coordinates. This method takes 4 arguments: Latitude 1, Longitude 1, Latitude 2, Longitude 2.
+	##
 
 	def map(latitude_min, longitude_min, latitude_max, longitude_max)
 		latlng = latitude_min.to_s + longitude_min.to_s + latitude_max.to_s + longitude_max.to_s
@@ -55,6 +65,10 @@ class AirGem
 		end
 	end
 
+	##
+	# => Returns data about stations near the searched keyword.
+	##
+
 	def search(keyword)
 		data = JSON.parse(Net::HTTP.get('api.waqi.info', "/search/?keyword=#{keyword}&token=#{@token}"))
 		if data['status'] == 'error'
@@ -63,5 +77,5 @@ class AirGem
 			return data
 		end
 	end
-	
+
 end
--- pollution.gemspec
+++ pollution.gemspec
@@ -6,7 +6,7 @@ Gem::Specification.new do |s|
   s.description = "This is a fast and easy-to-use library which makes it easy to make queries to the Air Pollution API. It makes it easy to send queries to the API and always returns easy to use hashes. It also manages errors. The complete API has been implemented!"
   s.authors     = ["Cosimo Libutti"]
   s.email       = 'cos.libutti@gmail.com'
-  s.files       = ["lib/airgem.rb"]
+  s.files       = ["lib/pollution.rb"]
   s.homepage    = 'https://rubygems.org/gems/example'
   s.metadata    = { "source_code_uri" => "https://github.com/example/example", "api_docs" => "https://aqicn.org/json-api/doc/" }
 end
